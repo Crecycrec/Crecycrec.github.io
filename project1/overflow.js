6ff1a84402d90ba16cd481a1891cfaf34da8ef16
LelCrec <168678416+Crecycrec@users.noreply.github.com>
2025-04-29 15:40:25 -0700

    Pages changes yippe

---
+++ project1/overflow.js
@@ -0,0 +1,56 @@
+const imageData =[
+    {id: "0", className: "", alt: ""},
+    {id: "1", className: "", alt: ""},
+    {id: "2", className: "", alt: ""},
+    {id: "3", className: "", alt: ""},
+    {id: "4", className: "", alt: ""},
+    {id: "5", className: "", alt: ""},
+    {id: "6", className: "", alt: ""},
+    {id: "7", className: "", alt: ""},
+    {id: "8", className: "", alt: ""},
+    {id: "9", className: "", alt: ""},
+    {id: "10", className: "", alt: ""},
+    {id: "11", className: "", alt: ""},
+    {id: "12", className: "", alt: ""},
+    {id: "13", className: "", alt: ""},
+    {id: "14", className: "", alt: ""},
+    {id: "15", className: "", alt: ""}
+];
+
+let currentIndex = 0;
+const carousel = document.getElementById("carousel");
+const carousel2 = document.getElementById("carousel2");
+const words = document.getElementById("words");
+let atoplayInterval = null;
+let isPlaying = false;
+
+//Functions that shows the new image in the carousel.
+function showImage(index){
+    carousel.style.opacity = 1;
+    carousel.src = `overflow/Overflow-${imageData[index].id}.png`;
+    carousel.alt = imageData[index].alt;
+    carousel.className= imageData[index].className;
+    words.className= imageData[index].className;
+}
+
+function showImage2(index){
+    carousel2.style.opacity = 1;
+    carousel2.src = `overflow/Overflow-${imageData[index+1].id}.png`;
+    carousel2.alt = imageData[index].alt;
+    carousel2.className= imageData[index].className;
+    words.className= imageData[index].className;
+}
+
+function nextImage(){
+    currentIndex= (currentIndex+2) % imageData.length;
+    showImage(currentIndex);
+    showImage2(currentIndex);
+}
+function prevImage(){
+    currentIndex=(currentIndex-2+imageData.length) % imageData.length;
+    showImage(currentIndex);
+    showImage2(currentIndex);
+}
+
+showImage(currentIndex);
+showImage2(currentIndex);
