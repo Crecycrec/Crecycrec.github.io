e50000c7b0afd7ad4e5f0bb823af0d3d8f71c847
LelCrec <168678416+Crecycrec@users.noreply.github.com>
2025-04-29 21:17:06 -0700

    I figured out how to add text
    my next step is to do the opacity thingy mabob

--- project1/overflow.js
+++ project1/overflow.js
@@ -1,9 +1,9 @@
 const imageData =[
-    {id: "0", className: "", alt: ""},
-    {id: "1", className: "txt1", alt: ""},
-    {id: "2", className: "", alt: ""},
-    {id: "3", className: "", alt: ""},
-    {id: "4", className: "", alt: ""},
+    {id: "0", className: "txtr", alt: ""},
+    {id: "1", className: "txtl", alt: ""},
+    {id: "2", className: "txtl", alt: ""},
+    {id: "3", className: "txtr", alt: ""},
+    {id: "4", className: "txtr", alt: ""},
     {id: "5", className: "", alt: ""},
     {id: "6", className: "", alt: ""},
     {id: "7", className: "", alt: ""},
@@ -40,6 +40,15 @@ let currentIndex = 0;
 const carousel = document.getElementById("carousel");
 const carousel2 = document.getElementById("carousel2");
 const words = document.getElementById("words");
+const words2 = document.getElementById("words2");
+const words3 = document.getElementById("words3");
+const words4 = document.getElementById("words4");
+const words5 = document.getElementById("words5");
+const words6 = document.getElementById("words6");
+const words7 = document.getElementById("words7");
+const words8 = document.getElementById("words8");
+const words9 = document.getElementById("words9");
+var arrowL = document.getElementById("al");
 let atoplayInterval = null;
 let isPlaying = false;
 
@@ -48,14 +57,57 @@ function showImage(index){
     carousel.style.opacity = 1;
     carousel.src = `overflow/Overflow-${imageData[index].id}.png`;
     carousel.alt = imageData[index].alt;
-    words.className= imageData[index].className;
+    // words.className= imageData[index].className;
+    // words2.className= imageData[index].className;
+    // words3.className= imageData[index].className;
+    // words4.className= imageData[index].className;
+    // words5.className= imageData[index].className;
+    // words6.className= imageData[index].className;
+    // words7.className= imageData[index].className;
+    // words8.className= imageData[index].className;
+    // words9.className= imageData[index].className;
+    words.setAttribute('src', '');
+    words2.setAttribute('src', '');
+    words3.setAttribute('src', '');
+    words4.setAttribute('src', '');
+    words5.setAttribute('src', '');
+    words6.setAttribute('src', '');
+    words7.setAttribute('src', '');
+    words8.setAttribute('src', '');
+    words9.setAttribute('src', '');
+
+    if(currentIndex == 0){
+        arrowL.setAttribute('disabled', true);
+        arrowL.style.opacity = 0;
+    } else if(currentIndex > 0){
+        arrowL.removeAttribute('disabled');
+        arrowL.style.opacity = 1;
+    }
+
+    if(currentIndex + 1 == 1){
+        words.className= imageData[index].className;
+        words.src = `overflow/Overflowtxt-${imageTxt[1].id}.png`;
+    } else if(currentIndex == 2){
+        words.className= imageData[index].className;
+        words2.className= imageData[index].className;
+        words3.className= imageData[index].className;
+        words4.className= imageData[index].className;
+        words.src = `overflow/Overflowtxt-${imageTxt[2].id}.png`;
+        words2.src = `overflow/Overflowtxt-${imageTxt[3].id}.png`;
+        words3.src = `overflow/Overflowtxt-${imageTxt[4].id}.png`;
+        words4.src = `overflow/Overflowtxt-${imageTxt[5].id}.png`;
+    } else{
+        // words.setAttribute('src', '');
+        // words2.setAttribute('src', '');
+        // words2.setAttribute('src', '');
+    }
 }
 
 function showImage2(index){
     carousel2.style.opacity = 1;
     carousel2.src = `overflow/Overflow-${imageData[index+1].id}.png`;
     carousel2.alt = imageData[index+1].alt;
-    words.className= imageData[index+1].className;
+    // words.className= imageData[index+1].className;
 }
 
 function nextImage(){
@@ -67,13 +119,10 @@ function prevImage(){
     currentIndex=(currentIndex-2+imageData.length) % imageData.length;
     showImage(currentIndex);
     showImage2(currentIndex);
+    
 }
 
-if(currentIndex + 1 == 1){
-    words.src = `overflow/Overflowtxt-${imageTxt[1].id}.png`;
-} else{
-    words.src = ``;
-}
+
 
 showImage(currentIndex);
 showImage2(currentIndex);
